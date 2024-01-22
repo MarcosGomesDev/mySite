10bac17619d2ace5fbffec1b5f570867fe1ad247
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-22 13:18:15 -0300

    edit layout header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,49 +1,66 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import DarkModeSwitcher from "../DarkModeSwitcher";
 import { Github, Instagram, Linkedin } from "lucide-react";
+import Logo from "../../../public/logo.svg";
 
 export function Header() {
   return (
-    <header className="flex items-center w-full h-20">
-      <div className="w-full max-w-[1200px] px-[15px] mx-auto">
-        <div>
-          <div className="flex items-center gap-14 justify-between">
-            <Image src="/vercel.svg" alt="logo" width={150} height={150} />
-            <ul className="flex items-center gap-10">
+    <header className='flex items-center w-full h-20'>
+      <div className='w-full container px-[15px] mx-auto'>
+        <div className=''>
+          <div className='flex items-center gap-14 justify-between'>
+            <Image
+              src={Logo}
+              alt='logo'
+              width={150}
+              height={150}
+              className='self-end mt-8'
+            />
+            <ul className='flex items-center gap-10 text-gray dark:text-gray-200 font-medium'>
               <li>
-                <Link href="#" className="gap-3">
+                <Link href='#' className='gap-3 hover:opacity-80'>
                   Home
                 </Link>
               </li>
               <li>
-                <Link href="#" className="gap-3">
+                <Link href='#' className='gap-3 hover:opacity-80'>
                   About
                 </Link>
               </li>
               <li>
-                <Link href="#" className="gap-3">
+                <Link href='#' className='gap-3 hover:opacity-80'>
                   Tech Stack
                 </Link>
               </li>
               <li>
-                <Link href="#" className="gap-3">
+                <Link href='#' className='gap-3 hover:opacity-80'>
                   Projects
                 </Link>
               </li>
               <li>
-                <Link href="#" className="gap-3">
+                <Link href='#' className='gap-3 hover:opacity-80'>
                   Contact
                 </Link>
               </li>
-              <li className="flex gap-4">
-                <Link href="#" className="gap-3 mr-2">
+              <li className='flex gap-4'>
+                <Link
+                  href='https://github.com/MarcosGomesDev'
+                  className='gap-3 mr-2 hover:opacity-80'
+                >
                   <Github size={24} />
                 </Link>
-                <Link href="#" className="gap-3 mr-2">
+                <Link
+                  href='https://www.linkedin.com/in/marcos-gomes-dev/'
+                  className='gap-3 mr-2 hover:opacity-80'
+                >
                   <Linkedin size={24} />
                 </Link>
-                <Link href="#" className="gap-3">
+                <Link
+                  href='https://www.instagram.com/'
+                  className='gap-3 hover:opacity-80'
+                >
                   <Instagram size={24} />
                 </Link>
               </li>
