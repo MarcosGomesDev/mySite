71ec7f3b0ffb3636f9672c87d2d61673a660454c
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-22 13:17:47 -0300

    fix layout switch theme mode

--- src/components/DarkModeSwitcher/index.tsx
+++ src/components/DarkModeSwitcher/index.tsx
@@ -38,8 +38,8 @@ function DarkModeSwitcher({ className }: DarkModeSwitcherProps) {
             "border w-[44px] h-[24px] p-px outline-none rounded-full relative cursor-pointer",
             "before:content-[''] before:w-[22px] before:h-[22px] before:transition-all before:duration-200 before:shadow-[1px_1px_3px_rgba(0,0,0,0.25)] before:absolute before:inset-y-0 before:my-auto before:rounded-full",
             {
-              "bg-primary border-white": darkMode,
-              "before:ml-[19px] before:bg-white ": darkMode,
+              "bg-primary border-gray-200": darkMode,
+              "before:ml-[19px] before:bg-white border-gray": darkMode,
             },
           ])}
         ></div>
