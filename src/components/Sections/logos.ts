58b2a00fa0fa47876b043c32ca1ab8e01be21978
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-23 14:50:51 -0300

    create array logos

---
+++ src/components/Sections/logos.ts
@@ -0,0 +1,78 @@
+import Git from "../../../public/git.svg";
+import Github from "../../../public/github.svg";
+import React from "../../../public/react.svg";
+import Next from "../../../public/next.svg";
+import Tailwind from "../../../public/tailwind.svg";
+import Redux from "../../../public/redux.svg";
+import Vercel from "../../../public/vercel.svg";
+import Typescript from "../../../public/typescript.svg";
+import Node from "../../../public/node.svg";
+import Nest from "../../../public/nest.svg";
+import Zustand from "../../../public/zustand.svg";
+import Javascript from "../../../public/javascript.svg";
+import Firebase from "../../../public/firebase.svg";
+import Mongo from "../../../public/mongo.svg";
+import AppCenter from "../../../public/appcenter.svg";
+
+export const logos = [
+  {
+    name: "Git",
+    src: Git,
+  },
+  {
+    name: "Github",
+    src: Github,
+  },
+  {
+    name: "React",
+    src: React,
+  },
+  {
+    name: "Next",
+    src: Next,
+  },
+  {
+    name: "Tailwind",
+    src: Tailwind,
+  },
+  {
+    name: "Redux",
+    src: Redux,
+  },
+  {
+    name: "Vercel",
+    src: Vercel,
+  },
+  {
+    name: "Typescript",
+    src: Typescript,
+  },
+  {
+    name: "Node",
+    src: Node,
+  },
+  {
+    name: "Nest",
+    src: Nest,
+  },
+  {
+    name: "Zustand",
+    src: Zustand,
+  },
+  {
+    name: "Javascript",
+    src: Javascript,
+  },
+  {
+    name: "Firebase",
+    src: Firebase,
+  },
+  {
+    name: "Mongo",
+    src: Mongo,
+  },
+  {
+    name: "AppCenter",
+    src: AppCenter,
+  },
+];
