f5c137a263e54b0fd32b812e2fb58eb401be8e7c
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-24 09:48:42 -0300

    add menu mobile

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,15 +2,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import DarkModeSwitcher from "../DarkModeSwitcher";
-import { Github, Instagram, Linkedin } from "lucide-react";
+import { Github, Instagram, Linkedin, Menu, X } from "lucide-react";
 import Logo from "../../../public/logo.svg";
 import { twMerge } from "tailwind-merge";
+import { useState } from "react";
 
 const arrayMenu = ["Inicio", "Sobre", "Tecnologias", "Projetos", "Contato"];
 
 export function Header() {
   const activeStyle =
     "text-transparent bg-clip-text bg-gradient-to-r from-[#ff0080] to-[#00C0FD]";
+
+  const [nav, setNav] = useState(false);
 
   return (
     <header className='flex items-center w-full h-20'>
@@ -22,7 +25,7 @@ export function Header() {
               alt='logo'
               className='w-24 h-auto object-contain'
             />
-            <ul className='hidden lg:flex items-center gap-10 text-gray dark:text-gray-200 font-semibold '>
+            <ul className='hidden lg:flex items-center gap-10 text-gray dark:text-gray-200 font-semibold'>
               {arrayMenu.map((item, index) => (
                 <li key={item}>
                   <Link
@@ -64,6 +67,60 @@ export function Header() {
                 <DarkModeSwitcher />
               </li>
             </ul>
+            <div
+              onClick={() => setNav(!nav)}
+              className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'
+            >
+              {nav ? (
+                <X size={30} className='text-blue dark:text-white' />
+              ) : (
+                <Menu size={30} className='text-blue dark:text-white' />
+              )}
+            </div>
+
+            {nav && (
+              <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-white dark:bg-black text-gray-500 text-2xl'>
+                {arrayMenu.map((item, index) => (
+                  <li key={item} className='mb-2'>
+                    <Link
+                      href='#'
+                      className={twMerge([
+                        `gap-3 hover:opacity-80`,
+                        `${index === 0 ? activeStyle : ""}`,
+                      ])}
+                    >
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+                <li className='flex gap-4 mt-6'>
+                  <Link
+                    href='https://github.com/MarcosGomesDev'
+                    target='_blank'
+                    className='gap-3 mr-2 hover:opacity-80'
+                  >
+                    <Github size={24} />
+                  </Link>
+                  <Link
+                    href='https://www.linkedin.com/in/marcos-gomes-dev/'
+                    target='_blank'
+                    className='gap-3 mr-2 hover:opacity-80'
+                  >
+                    <Linkedin size={24} />
+                  </Link>
+                  <Link
+                    href='https://www.instagram.com/'
+                    target='_blank'
+                    className='gap-3 hover:opacity-80'
+                  >
+                    <Instagram size={24} />
+                  </Link>
+                </li>
+                <li className='mt-5'>
+                  <DarkModeSwitcher />
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </div>
