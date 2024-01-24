23346422708a66c9aba9b3b3b78716dc8acb78f2
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-24 14:43:31 -0300

    add prop footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,62 +1,66 @@
+"use client";
 import Image from "next/image";
 import Logo from "../../../public/logo.svg";
 import Link from "next/link";
 import { Github, Instagram, Linkedin, Mail } from "lucide-react";
+import { Element } from "react-scroll";
 
 export function Footer() {
   return (
-    <footer className='h-20 w-full'>
-      <div className='w-full container px-[15px] mx-auto pb-12'>
-        <div className='border-b-2 border-b-gray-200'>
-          <div className='flex items-center justify-between'>
-            <Image
-              src={Logo}
-              alt='logo'
-              className='w-24 h-auto object-contain'
-            />
-            <div className='flex gap-6'>
-              <p className='hidden md:flex text-blue dark:text-gray-200'>
-                marcosgomesdev@gmail.com
-              </p>
-              <ul className='flex items-center text-blue dark:text-gray-200 font-medium mr-4'>
-                <li className='flex'>
-                  <Link
-                    href='https://github.com/MarcosGomesDev'
-                    target='_blank'
-                    className='gap-3 mr-2 hover:opacity-80 md:hidden flex'
-                  >
-                    <Mail size={24} />
-                  </Link>
-                  <Link
-                    href='https://github.com/MarcosGomesDev'
-                    target='_blank'
-                    className='gap-3 mr-2 hover:opacity-80'
-                  >
-                    <Github size={24} />
-                  </Link>
-                  <Link
-                    href='https://www.linkedin.com/in/marcos-gomes-dev/'
-                    target='_blank'
-                    className='gap-3 mr-2 hover:opacity-80'
-                  >
-                    <Linkedin size={24} />
-                  </Link>
-                  <Link
-                    href='https://www.instagram.com/'
-                    target='_blank'
-                    className='gap-3 hover:opacity-80'
-                  >
-                    <Instagram size={24} />
-                  </Link>
-                </li>
-              </ul>
+    <Element name='contact'>
+      <footer className='h-20 w-full'>
+        <div className='w-full container px-[15px] mx-auto pb-12'>
+          <div className='border-b-2 border-b-gray-200'>
+            <div className='flex items-center justify-between'>
+              <Image
+                src={Logo}
+                alt='logo'
+                className='w-24 h-auto object-contain'
+              />
+              <div className='flex gap-6'>
+                <p className='hidden md:flex text-blue dark:text-gray-200'>
+                  marcosgomesdev@gmail.com
+                </p>
+                <ul className='flex items-center text-blue dark:text-gray-200 font-medium mr-4'>
+                  <li className='flex'>
+                    <Link
+                      href='https://github.com/MarcosGomesDev'
+                      target='_blank'
+                      className='gap-3 mr-2 hover:opacity-80 md:hidden flex'
+                    >
+                      <Mail size={24} />
+                    </Link>
+                    <Link
+                      href='https://github.com/MarcosGomesDev'
+                      target='_blank'
+                      className='gap-3 mr-2 hover:opacity-80'
+                    >
+                      <Github size={24} />
+                    </Link>
+                    <Link
+                      href='https://www.linkedin.com/in/marcos-gomes-dev/'
+                      target='_blank'
+                      className='gap-3 mr-2 hover:opacity-80'
+                    >
+                      <Linkedin size={24} />
+                    </Link>
+                    <Link
+                      href='https://www.instagram.com/'
+                      target='_blank'
+                      className='gap-3 hover:opacity-80'
+                    >
+                      <Instagram size={24} />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
+          <p className='text-gray dark:text-gray-200 text-center mt-8'>
+            Marcos Gomes © 2023
+          </p>
         </div>
-        <p className='text-gray dark:text-gray-200 text-center mt-8'>
-          Marcos Gomes © 2023
-        </p>
-      </div>
-    </footer>
+      </footer>
+    </Element>
   );
 }
