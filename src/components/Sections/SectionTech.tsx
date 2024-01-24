976c4f7231405be4d3bc44984b6e95a3f2958265
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-24 09:37:54 -0300

    fix section tech

--- src/components/Sections/SectionTech.tsx
+++ src/components/Sections/SectionTech.tsx
@@ -10,14 +10,14 @@ export const SectionTech = () => {
           <p className='mt-6 text-3xl font-light text-gray-200'>
             Tecnologias que trabalhei recentemente
           </p>
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-24 gap-8 items-center justify-center'>
+          <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-24 gap-8 justify-center'>
             {logos.map((logo) => (
               <Image
                 key={logo.name}
                 src={logo.src}
                 alt={logo.name}
                 title={logo.name}
-                className='object-contain aspect-[4/3] w-32 h-auto'
+                className='object-contain aspect-[4/3] w-32 h-auto md:ml-6'
               />
             ))}
           </div>
