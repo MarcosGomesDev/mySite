b2a0de160691f178f225695c035d65cbcc3705fe
Marcos Gomes <marcosgomesdev@gmail.com>
2024-01-26 20:06:38 -0300

    add perfil photo

--- src/components/Sections/SectionHero.tsx
+++ src/components/Sections/SectionHero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Person from "../../../public/person.png";
+import Person from "../../../public/perfil.jpg";
 import { Element } from "react-scroll";
 
 export const SectionHero = () => {
@@ -19,7 +19,7 @@ export const SectionHero = () => {
           </div>
           <div className='rounded-full flex items-center justify-center w-72  bg-gradient-b-border-gradient h-72'>
             <div className='bg-white dark:bg-[#191919]  inset-0 w-[17rem] h-[17rem] rounded-full flex items-center justify-center'>
-              <Image src={Person} alt='hero' className='w-40 h-auto' />
+              <Image src={Person} alt='hero' className='rounded-full' />
             </div>
           </div>
         </div>
